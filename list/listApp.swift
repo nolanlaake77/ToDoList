dd9033a6ea24da33a2707bbe0c63c494adfa3a1e
Nolan <iamchunkerdunkers@gmail.com>
2024-02-15 10:08:45 -0700

    Initial Commit

---
+++ list/listApp.swift
@@ -0,0 +1,17 @@
+//
+//  listApp.swift
+//  list
+//
+//  Created by Nolan Laake on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
